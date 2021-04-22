53e0d60b412e5c00bb5aecce1fe0200735488cda
luciaberon <luciaberonwaingart@hotmail.com>
2021-04-22 08:44:39 -0300

    Loading spinner added to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import NewPost from './NewPost';
 import Axios from 'axios';
+import { Container, Spinner} from 'react-bootstrap';
 
 export default function Home() {
 
     const [ blogList, addBlog ] = useState(null);
+    const [loading, showLoad] = useState(true);
 
     useEffect(() => {    
         Axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
             addBlog(res.data);
+            showLoad(!loading);
         })
         .catch(err => {
             console.log(err);
@@ -19,14 +22,22 @@ export default function Home() {
 
 
     return (
-        <div className="mt-3 w-50 mx-auto">
-            <div>
-                <h3 className="text-center mt-4 mb-4">Posts</h3>
-                <NewPost />
-            </div>
-            
+        <div>
+        {loading ?
+        <Container className="d-flex align-items-center mt-5">
+            <Spinner animation="border mx-auto" role="status">
+                <span className="sr-only">Loading...</span>
+            </Spinner> 
+        </Container>
+        :
+        <div className="mt-3 w-50 mx-auto">            
+            <h3 className="text-center mt-4 mb-4">Posts</h3>
+            <NewPost />           
+
             { blogList && <BlogList blogList={blogList} />}
  
+        </div>
+        }
         </div>
     )
 }
