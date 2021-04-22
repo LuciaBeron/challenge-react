c6ad8bda9976d5788d00af39379372de0335b0eb
luciaberon <luciaberonwaingart@hotmail.com>
2021-04-22 02:45:13 -0300

    Popup modal to add post with api call

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
-
+import NewPost from './NewPost';
 import Axios from 'axios';
 
 export default function Home() {
@@ -8,7 +8,6 @@ export default function Home() {
     const [ blogList, addBlog ] = useState(null);
 
     useEffect(() => {    
-
         Axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
             addBlog(res.data);
@@ -16,22 +15,26 @@ export default function Home() {
         .catch(err => {
             console.log(err);
         })
-
     }, [blogList])
 
 
     return (
         <div className="mt-3 w-50 mx-auto">
-            <h3 className="text-center mt-4 mb-4">Posts</h3>
+            <div>
+                <h3 className="text-center mt-4 mb-4">
+                    Posts
+                </h3>
+                <NewPost />
+            </div>
+
             <table className="table">
-
                 <tbody className="table">
-                { blogList && <BlogList blogList={blogList} />}
+                    { blogList && <BlogList blogList={blogList} />}
                 </tbody>
-
             </table>
+        
 
-                       
+
         </div>
     )
 }
---
+++ src/components/NewPost.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import { Form, Button, Modal, Alert } from 'react-bootstrap';
+import Axios from 'axios';
+
+export default function NewPost() {
+
+    const [number, posts] = useState(101);
+    const [show, setShow] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+ 
+    const [form, setForm] = useState({
+      title:'',
+      body: ''
+    })
+
+    const updateForm = e => {
+      setForm({
+        ...form,
+        [e.target.name] : e.target.value,
+      })
+    }
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      const validate = validateForm();
+   
+      if (validate) {
+        Axios.post('https://jsonplaceholder.typicode.com/posts',
+          {
+            data: {
+              title: form.title,
+              body: form.body,
+              id:number
+          }})
+        .then(res => {
+          console.log(res);
+          posts(number++);
+        })
+        .catch(err => console.log(err));
+
+      }
+    }
+
+    const validateForm = e => {
+      const { title, body } = form;
+
+      if (title.length === 0 || body.length === 0) {
+          setError('All fields are required');
+          return false;
+      } else {
+        setError('');
+      }
+      return true;
+    }
+  
+    return (
+      <>
+        <Button className="d-block mx-auto mb-4" onClick={handleShow}>
+          Add post
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+        <Form className="p-3">            
+            <Modal.Header closeButton>
+                <Modal.Title>New post</Modal.Title>
+            </Modal.Header>        
+          
+            <Form.Group controlId="formTitle">
+                <Form.Label className="pt-3">Title</Form.Label>
+                <Form.Control onChange={updateForm} name="title" type="text" placeholder="Enter post title" />
+            </Form.Group>
+
+            <Form.Group controlId="formBody">
+                <Form.Label>Post content</Form.Label>
+                <Form.Control onChange={updateForm} name="body" as="textarea" rows={5} placeholder="Post content" />
+            </Form.Group>       
+
+            {error.length > 0 && <Alert variant={'danger'}>{error}</Alert>}
+
+            <Button type="submit" onClick={handleSubmit}>
+                Submit
+            </Button>
+          </Form>
+        </Modal>
+      </>
+    );
+  }
+       
+
+
